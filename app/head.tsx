7773300b14473bb4699740de51a1a563345c6751
Kaykov Media <kaykovmedia@Kaykovs-Mac-mini-3.local>
2026-01-28 10:22:24 -0500

    Add explicit favicon link tags

---
+++ app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <link rel="icon" href="/favicon.ico" sizes="any" />
+      <link rel="icon" type="image/png" href="/favicon.png" />
+      <link rel="apple-touch-icon" href="/apple-icon.png" />
+    </>
+  )
+}
